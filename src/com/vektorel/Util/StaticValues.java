0a0848e89e0451a294649f32553cd3716d6aa91f
vektorel <vektorel@lab3-ogr>
2019-06-01 09:49:46 +0300

    Örnek form tasarım

---
+++ src/com/vektorel/Util/StaticValues.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vektorel.Util;
+
+/**
+ *
+ * @author vektorel
+ */
+public class StaticValues {
+    
+    public static final String userName = "admin";
+    public static final String password = "12345";
+    
+}
